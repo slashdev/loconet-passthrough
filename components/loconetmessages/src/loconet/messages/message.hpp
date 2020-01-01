47f5e8a4c6ab37819a76debc671f33656906a480
Jan Martijn van der Werf <janmartijn@vdwerf.eu>
2020-01-01 22:01:24 +0100

    Move opcodes to its own file

--- components/loconetmessages/src/loconet/messages/message.hpp
+++ components/loconetmessages/src/loconet/messages/message.hpp
@@ -18,45 +18,12 @@
 // Required for all the types being used
 #include "esp_system.h"
 
+#include "opcodes.hpp"
+
 namespace loconet
 {
 	namespace messages
 	{
-
-		typedef enum OPCODE : uint8_t
-		{
-			// 2 byte messages
-				BUSY         = 0x81,
-				GPOFF        = 0x82,
-				GPON         = 0x83,
-				IDLE         = 0x85,
-
-			// 4 byte messages
-				LOCO_SPD     = 0xA0,
-				LOCO_DIRF    = 0xA1,
-				LOCO_SND     = 0xA2,
-				SW_REQ       = 0xB0,
-				SW_REP       = 0xB1,
-				INPUT_REP    = 0xB2,
-				LONG_ACK     = 0xB4,
-				SLOT_STAT1   = 0xB5,
-				CONSIST_FUNC = 0xB6,
-				UNLINK_SLOTS = 0xB8,
-				LINK_SLOTS   = 0xB9,
-				MOVE_SLOTS   = 0xBA,
-				RQ_SL_DATA   = 0xBB,
-				SW_STATE     = 0xBC,
-				SW_ACK       = 0xBD,
-				LOCO_ADR     = 0xBF,
-
-			// 6 byte messages
-
-			// variable length
-				PEER_XFER    = 0xE5,
-  			RD_SL_DATA   = 0xE7,
-  			IMM_PACKET   = 0xED,
-  			WR_SL_DATA   = 0xEF
-  	} eOPCode_t;
 
 		class Message
 		{
