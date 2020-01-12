71f27b184a813089275a386594159e50588fd76d
Jan Martijn van der Werf <janmartijn@vdwerf.eu>
2020-01-12 16:58:02 +0100

    Initial WifiConnector page

---
+++ components/wifi_httpdhandler/src/wifihandler/handler.hpp
@@ -0,0 +1,35 @@
+#ifndef WIFIREQUEST_HANDLER_HPP_
+#define WIFIREQUEST_HANDLER_HPP_
+
+#include <string>
+#include "httpd/requesthandler.hpp"
+#include "httpd/server.hpp"
+
+namespace httpd
+{
+  namespace handlers
+  {
+    namespace wifi
+    {
+      class Handler : public httpd::RequestHandler
+      {
+      public:
+        Handler(httpd::Server*);
+        bool accept(httpd::Request*);
+        void handle(httpd::Request*, httpd::Response*);
+
+        void GET_url(std::string);
+        std::string GET_url(void);
+
+        void POST_url(std::string);
+        std::string POST_url(void);
+      private:
+        httpd::Server *server_ = NULL;
+        std::string get_url_ = "/wifi";
+        std::string post_url_ = "/wifi/set";
+      };
+    }
+  }
+}
+
+#endif
